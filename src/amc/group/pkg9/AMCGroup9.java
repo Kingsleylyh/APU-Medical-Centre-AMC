8c47f95e1beab2d43e2a7cbf5b18f1815e2a5229
Kingsley <kingsleylyh0918@gmail.com>
2025-07-09 13:17:00 +0800

    test

--- src/amc/group/pkg9/AMCGroup9.java
+++ src/amc/group/pkg9/AMCGroup9.java
@@ -14,7 +14,7 @@ public class AMCGroup9 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello World! Welcome to the project!");
     }
     
 }
