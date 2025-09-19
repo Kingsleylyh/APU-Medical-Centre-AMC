831a70f9f2dfa87ca7f5d588f4aa76eb2c6bb711
blblzwm <tevychong@gmail.com>
2025-09-19 17:33:57 +0800

    Remove user.txt from remote tracking

--- src/amc/group/pkg9/AMCGroup9.java
+++ src/amc/group/pkg9/AMCGroup9.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package amc.group.pkg9;
 
-/**
- *
- * @author Lucky
- */
 public class AMCGroup9 {
 
     /**
