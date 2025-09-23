d6eae847cd2a8f13d631183909edafe1cf204b1a
blblzwm <tevychong@gmail.com>
2025-09-24 01:04:34 +0800

    add backIcon.png

--- src/amc/group/pkg9/PrescriptionGUI.java
+++ src/amc/group/pkg9/PrescriptionGUI.java
@@ -247,7 +247,7 @@ public class PrescriptionGUI extends javax.swing.JFrame {
 
         jPanel2.setBackground(new java.awt.Color(233, 226, 219));
 
-        jButton2.setIcon(new javax.swing.ImageIcon("C:\\Users\\User\\Downloads\\left (1).png")); // NOI18N
+        jButton2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/amc/group/pkg9/backIcon.png"))); // NOI18N
         jButton2.setBorder(null);
         jButton2.setBorderPainted(false);
         jButton2.setContentAreaFilled(false);
